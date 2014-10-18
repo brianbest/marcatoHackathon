efc22f1163247004cedb79bd25193b999dbc5a1d
clouie87 <carol.louie@gmail.com>
2014-10-18 19:55:09 -0300

    instructors page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+var answer = '2'
+
 $('form').submit(function(){
 	event.preventDefault();
-	var answer = document.getElementById("txtans").value;
-	console.log("answer is" +answer);
-	var text = document.createTextNode(answer);
+	var txtanswer = document.getElementById("txtans").value;
+	console.log("user answer is" +txtanswer);
+	var text = document.createTextNode(txtanswer);	
 	document.getElementById("answerbox").appendChild(text);
-
+	console.log("text is " + text);
+	if(text !== answer){
+		console.log("wow they suck");
+		alert("Too bad!");
+	}else{
+		console.log("Correct!");
+		alert("Awesome job!");
+	};
 	document.getElementById("txtans").value="";
+	
 });
+

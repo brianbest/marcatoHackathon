f2fe7b14c21113646f65142f117f554429dabba3
Brian best <brian.brianb64@gmail.com>
2014-10-18 13:51:57 -0300

    Start of Proof

---
+++ script.js
@@ -0,0 +1,51 @@
+
+//Global Vars
+//===================================================
+var final_transcript ='';
+
+
+//===================================================
+
+//The Magic area!
+//===================================================
+if (!('webkitSpeechRecognition' in window)) {
+  upgrade();
+} else {
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = false;
+  //recognition.interimResults = true;
+
+  //recognition.onstart = function() { ... }
+  recognition.onresult = function(event) {
+    var interim_transcript = event;
+    console.log(interim_transcript);
+  };
+  recognition.onend = function() {
+    if (recognizing) {
+      recognition.stop();
+      return;
+    }
+  };
+  recognition.onerror = function(event) {
+    var interim_transcript = event;
+    console.log("error: "interim_transcript);
+  };
+}
+
+//===================================================
+
+
+//Functions of awesome
+//===================================================
+function startButton(event) {
+
+  final_transcript = '';
+  recognition.lang = "en-US";//select_dialect.value;
+  recognition.start();
+}
+
+function upgrade(){
+  alert('Why do you hate me?');
+}
+
+//===================================================

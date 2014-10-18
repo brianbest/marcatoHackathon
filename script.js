e4809ced27b59ff01b7701e5b64734138fa1c21e
Brian best <brian.brianb64@gmail.com>
2014-10-18 16:55:02 -0300

    fix for change text responce

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
-
 //Global Vars
 //===================================================
-var final_transcript ='';
-var transcript ='';
 var question = '1+1= ?';
 var answer = '2';
 
+//function Vars
 //===================================================
+var final_transcript ='';//for speech
+var transcript ='';// for speech
 
-//The Magic area!
-//===================================================
+//check if speech to text is available
 if (!('webkitSpeechRecognition' in window)) {
   upgrade();
 } else {
   var recognition = new webkitSpeechRecognition();
-  recognition.continuous = false;
-  //recognition.interimResults = true;
-
-  //recognition.onstart = function() { ... }
-  recognition.onresult = function(event) {
-    transcript = event.results[0][0].transcript;
-    document.getElementById('capText').html = transcript;
-  };
-  recognition.onend = function() {
-    recognition.stop();
-    if (transcript === answer){
-      var msg = new SpeechSynthesisUtterance('correct!');
-        window.speechSynthesis.speak(msg);
-      alert('Right!');
-    }else{
-      var msg = new SpeechSynthesisUtterance('wrong!');
-        window.speechSynthesis.speak(msg);
-      alert('WRONG! -- GTFO!');
-    }
-  };
-  recognition.onerror = function(event) {
-    var interim_transcript = event;
-    console.log("error: " + interim_transcript);
-  };
 }
 
 //===================================================
 
 
-//Functions of awesome
+speechQuestion(question, answer);
+
+function speechQuestion(q, a){
+
+  var asked_question = q;
+  var asked_answer = a;
+
+  //The Magic area!
+  //===================================================
+    recognition.continuous = false;
+    recognition.onresult = function(event) {
+      transcript = event.results[0][0].transcript;
+      document.getElementById('capText').html(transcript);
+    };
+    recognition.onend = function() {
+      recognition.stop();
+      if (transcript === asked_answer){
+        var msg = new SpeechSynthesisUtterance('correct!');
+          window.speechSynthesis.speak(msg);
+        alert('Right!');
+      }else{
+        var msg = new SpeechSynthesisUtterance('wrong!');
+          window.speechSynthesis.speak(msg);
+        alert('WRONG! -- GTFO!');
+      }
+    };
+    recognition.onerror = function(event) {
+      var interim_transcript = event;
+      console.log("error: " + interim_transcript);
+    };
+}
+
+//===================================================
+
+//Sub-Functions needed for speach
 //===================================================
 function startButton(event) {
 

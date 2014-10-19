f829a512eebe147dfcbaeb6d025942b621cb0862
Brian best <brian.brianb64@gmail.com>
2014-10-19 12:32:37 -0300

    bug fixes

--- js/func.js
+++ js/func.js
@@ -169,6 +169,11 @@ function speechQuestion(a){
 //===================================================
 function dragQuestion(a) {
 	$('.dragQuestion').removeClass('hide');
+	var width = $( window ).width();
+	if(width < 500){
+		mobileInit();
+	}
+
 
 	$('#answerbox').html('drag your answer here');
 
@@ -236,3 +241,30 @@ function dragQuestion(a) {
 	});
 }
 //===================================================
+
+
+//when on mobile
+
+function touchHandler(event) {
+    var touch = event.changedTouches[0];
+
+    var simulatedEvent = document.createEvent("MouseEvent");
+        simulatedEvent.initMouseEvent({
+        touchstart: "mousedown",
+        touchmove: "mousemove",
+        touchend: "mouseup"
+    }[event.type], true, true, window, 1,
+        touch.screenX, touch.screenY,
+        touch.clientX, touch.clientY, false,
+        false, false, false, 0, null);
+
+    touch.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+
+function mobileInit() {
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);
+}
